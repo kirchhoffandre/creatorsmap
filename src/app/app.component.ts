8aeae90979dae455ac820266e0a4512628b5a45c
Andre Kirchhoff <akirchhoff@akirchhoff-macbookpro2.roam.corp.google.com>
2019-02-23 11:54:34 +0000

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { slider, transformer, fader, stepper } from './route-animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    // fader,
+    // slider,
+    // transformer,
+    fader
+  ]
 })
 export class AppComponent {
   
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animations'];
+  }
 }
