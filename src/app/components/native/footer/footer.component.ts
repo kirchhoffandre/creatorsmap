5dc677ac90b67e9876c7b625bf27b28f188de4c0
Andre Kirchhoff <akirchhoff@akirchhoff-macbookpro2.roam.corp.google.com>
2019-03-01 14:46:07 +0000

    at least a commit

--- src/app/components/native/footer/footer.component.ts
+++ src/app/components/native/footer/footer.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fadeAnimation, fader } from '../../../route-animations';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations: [
+    // fader,
+    // slider,
+    // transformer,
+    fader
+  ]
 })
 export class FooterComponent implements OnInit {
   
